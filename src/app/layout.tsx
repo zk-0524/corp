af922a4e7be1a7b5e116e7ce96be43fb41c9bd01
zk-0524 <2634295761@qq.com>
2025-07-18 10:38:41 +0800

    删除字体

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { Tangerine } from "next/font/google"
 import "./globals.css";
 
 const tangerine = Tangerine({
@@ -29,7 +28,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={tangerine.className}>
+    <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
