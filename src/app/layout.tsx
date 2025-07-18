c29930a6cc247c6ade3cd7f241aeda48e069d26b
zk-0524 <2634295761@qq.com>
2025-07-18 10:50:23 +0800

    Update layout.tsx
    delete

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const tangerine = Tangerine({
-  // subsets: ["latin"],
-  weight: '400',
-})
-
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
